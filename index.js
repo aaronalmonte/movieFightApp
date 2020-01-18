fe87baa0a2f8a7b1ee3c0fdac6ba32ce46f76ae6
Aaron Almonte <aaron.almontes@gmail.com>
2020-01-18 00:46:52 -0500

    html: added input element. js: added eventListener to the input and created function to delay fetching data until the user stops typing for some time

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('https://omdbapi.com/', {
         params: {
             apikey: 'f992c7b7',
-            s: 'avengers'
+            s: searchTerm
         }
     });
     console.log (response.data);
 };
 
-fetchData();
+let timeoutId;
+const input = document.querySelector('input');
+
+onInput = event => {
+    if (timeoutId) {
+        clearTimeout (timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value)
+    }, 500);
+}
+input.addEventListener("input", onInput);
+
+//fetchData();
 
