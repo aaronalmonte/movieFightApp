684ba0d649f1de57aeb9db93ea74c34acfeb93a5
Aaron Almonte <aaron.almontes@gmail.com>
2020-01-17 23:10:27 -0500

    Created and tested arrow function to fetch movie data from https://omdbapi.com

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('https://omdbapi.com/', {
+        params: {
+            apikey: 'f992c7b7',
+            s: 'avengers'
+        }
+    });
+    console.log (response.data);
+};
+
+fetchData();
+
